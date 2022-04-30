be27697546f6903b2a69adc8051915e7b195d8d2
lgsdiamant <lgsdiamant@gmail.com>
2022-05-01 06:37:48 +0900

    Phase01: .ignore for build grade in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-// main: the starting of Enclave App
-import 'package:enclave/test.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'enclave_app.dart';
 import 'firebase_options.dart';
+
+///
+/// Starting of App
+///
 
 void main() async {
   await GetStorage.init();
